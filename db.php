246d4ed4f6e66d8e39a7f721b64bf12770d3868c
Afik Tahirov <afik.t38@gmail.com>
2024-07-01 13:42:14 +0400

    Updated database

--- db.php
+++ db.php
@@ -18,9 +18,13 @@ class  Database{
     
     public function insert($fname,$lname,$email,$phone){
         $sql = "INSERT INTO users (first_name,last_name,email,phone) VALUES (:fname,:lname,:email,:phone)";
-
         $stmt = $this->conn->prepare($sql);
         $stmt->execute(['fname'=>$fname,'lname'=>$lname,'email'=>$email,'phone'=>$phone]);
+        return true;
+    }
+
+    public function read(){
+        //
     }
 }
 
